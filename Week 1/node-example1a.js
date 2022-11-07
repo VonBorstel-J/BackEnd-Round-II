28fbd852684fbf5f4b1c8712c2f1a2d50433b1e9
Jordan VonBorstel <jordanvonborstel@gmail.com>
2022-11-07 14:39:58 -0800

    week 1 complete

--- Week 1/node-example1a.js
+++ Week 1/node-example1a.js
@@ -1,4 +1,5 @@
 var myLogModule = require('./node-example1.js'); // add ./ because module is in same directory
+myLogModule.info('Node.js started');
 
 myLogModule.info('Node.js started');
 myLogModule.warning('Node.js has a warning');
